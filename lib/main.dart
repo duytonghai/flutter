af0ee8f1685631d41d10bc44776270b38db0dde8
Duy <duy.tonghai@unified.com>
2018-06-30 14:41:24 +0700

    Add layout tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'words_list_view.dart';
-import 'new_view.dart';
+import 'package:flutter_app/words_list_view.dart';
+import 'package:flutter_app/floating_button.dart';
+import 'package:flutter_app/layout.dart';
 
 void main() => runApp(new MyApp());
 
@@ -11,21 +12,23 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 2,
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
           title: new Text('Dito App'),
           bottom: new TabBar(
             isScrollable: true,
             tabs: [
-              Tab(text: 'List View'),
-              Tab(text: 'Blank View')
+              Tab(icon: Icon(Icons.list)),
+              Tab(icon: Icon(Icons.photo)),
+              Tab(icon: Icon(Icons.flash_on)),
             ]),
         ),
         body: TabBarView(
           children: [
             RandomWords(),
-            NewViews(),
+            Layout(),
+            FloatingButton(),
           ],
         ),
       ),
